52aace60ce0d33412a33f869cb04b8764027309d
Ryzeon <alex.ramon2004@gmail.com>
2024-04-10 04:42:44 -0500

    feat(navbar): import colors to tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./src/**/*.{html,js}", "index.html"],
+    theme: {
+        colors: {
+            cc_1: "#00628d",
+            cc_2: "#337eab",
+            cc_3: "#529bca",
+            cc_4: "#72b9ea",
+            cc_5: "#96d1fb",
+            cc_6: "#bae0fc",
+            cc_7: "#dbeffe",
+            cc_8: "#fbffff",
+        }
+    },
+    plugins: [],
 }
